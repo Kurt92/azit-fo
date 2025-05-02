15732a00c1020ead532f812788374d07d6193f1c
kurt92 <chcch1234@gmail.com>
2025-05-03 04:36:38 +0900

    fix : 환경변수 추가 객체 롤백

--- next.config.js
+++ next.config.js
@@ -12,13 +12,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false, // Strict Mode 비활성화
-    // 이거 .evn 에 public 붙이면 자동으로 처리됨. 따라서 주석처리
-    // env: {
-    //     // .env 파일에서 환경 변수 가져오기
-    //     NEXT_AUTH_URL: process.env.NEXT_AUTH_URL,
-    //     NEXT_API_URL: process.env.NEXT_API_URL,
-    //     NEXT_GATEWAY_URL: process.env.NEXT_GATEWAY_URL,
-    // },
+    // .evn 에 public 붙이면 자동으로 처리됨. 따라서 주석처리
+    // 주석을 했는데 build 는 프로덕션이라서 자동으로 읽지 않는다고함(next dev에서만 자동적용) 그래서 해제함
+    env: {
+        // .env 파일에서 환경 변수 가져오기
+        NEXT_PUBLIC_AUTH_URL: process.env.NEXT_PUBLIC_AUTH_URL,
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        NEXT_PUBLIC_CHAT_URL: process.env.NEXT_PUBLIC_CHAT_URL,
+        NEXT_PUBLIC_GATEWAY_URL: process.env.NEXT_PUBLIC_GATEWAY_URL,
+    },
 };
 
 module.exports = nextConfig;
--- src/shared/util/ApiReq/user/req.tsx
+++ src/shared/util/ApiReq/user/req.tsx
@@ -4,7 +4,8 @@ import {IUser} from "@/shared/types/UserInterface";
 // 여기서 : 이게 들어가는 문법은 타입스크립트 문법으로 리턴값을 지정해주는 문법임
 // 따라라서 : 이거 뒤에오는 IUser의 프로미스 타입으로 데이터라 리턴되지 않으면 ide컴파일 에러(빨간줄)이 남. 나긴하지만 런타임에선 됨
 export const user = async (): Promise<IUser> => {
-    const response = await axios.get("/api/auth/token", {
+    const authDomain = process.env.NEXT_PUBLIC_AUTH_URL;
+    const response = await axios.get(`${authDomain}/auth/token`, {
         withCredentials: true,
     });
     return response.data.user; // 반환 데이터를 명확히 타입 지정
